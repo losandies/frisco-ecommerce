8882e4af43965578e0574d4f48545260d4572afd
Brandon Newsome <brandonn.dev@gmail.com>
2023-03-02 20:44:35 -0500

    built out logic for registering user, logging in user, getting all users, and deleting all users. also implemented jsonwebtokens for user authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,36 +1,109 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const registerUser = async (req, res) => {
-    const userInfo = {
-        name: "Brandon Newsome",
-        email: "brandon@gmail.com",
-        password: "123456",
-    };
+    const { name, email, password } = req.body;
 
     try {
-        const newUser = await prisma.user.create({
-            data: {
-                name: userInfo.name,
-                email: userInfo.email,
-                password: userInfo.password,
+        if (!(name || email || password)) {
+            res.status(400).json({ error: "Missing Fields" });
+        }
+
+        const userExists = await prisma.user.findFirst({
+            where: {
+                email: email,
             },
         });
-        if (newUser) {
-            res.json(newUser);
+
+        if (userExists) {
+            res.status(409).json({
+                error: "User Already Exists. Please Log In",
+            });
         }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await prisma.user.create({
+            data: {
+                name,
+                email: email.toLowerCase(),
+                password: encryptedPassword,
+            },
+        });
+
+        const token = jwt.sign({ id: newUser.id }, process.env.jwtSecret, {
+            expiresIn: "1hr",
+        });
+
+        newUser.token = token;
+
+        res.status(201).json(newUser);
     } catch (error) {
         console.log(error);
     }
 };
 
-const getUsers = async (req, res) => {
-    const allUsers = await prisma.user.findMany();
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
 
-    res.json(allUsers);
+    if (!(email || password)) {
+        res.status(400).json({ error: "Missing Fields" });
+    }
+
+    try {
+        const foundUser = await prisma.user.findFirst({
+            where: {
+                email: email,
+            },
+        });
+
+        if (!foundUser) {
+            res.status(400).json({
+                error: "User doesnt exist. Please register an account",
+            });
+        }
+
+        const match = await bcrypt.compare(password, foundUser.password);
+
+        if (!match) {
+            res.status(401).json({ error: "Invalid Credentials" });
+        }
+
+        const token = jwt.sign({ id: foundUser.id }, process.env.jwtSecret, {
+            expiresIn: "1hr",
+        });
+
+        foundUser.token = token;
+
+        res.status(201).json(foundUser);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const getUsers = async (req, res) => {
+    try {
+        const allUsers = await prisma.user.findMany();
+
+        if (allUsers.length > 1) {
+            res.json(allUsers);
+        } else if (allUsers.length < 1) {
+            res.status(400).json({ msg: "No users found" });
+        }
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const deleteAllUsers = async () => {
+    await prisma.user.deleteMany({});
 };
 
 module.exports = {
     registerUser,
     getUsers,
+    deleteAllUsers,
+    loginUser,
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { registerUser, getUsers } = require("../controllers/userController");
+const {
+    registerUser,
+    getUsers,
+    deleteAllUsers,
+    loginUser,
+} = require("../controllers/userController");
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.get("/allUsers", getUsers);
+router.delete("/delete", deleteAllUsers);
 
 module.exports = router;
