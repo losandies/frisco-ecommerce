0c6dab689e3eb875ad0fe78cf3544a8545a54449
Brandon Newsome <brandonn.dev@gmail.com>
2023-04-13 10:01:36 -0400

    added display of user info on my profile section of my account page

--- client/src/components/account/AccountInfoContainer.jsx
+++ client/src/components/account/AccountInfoContainer.jsx
@@ -3,20 +3,7 @@ import React, { Children } from "react";
 const AccountInfoContainer = ({ currentTab, children }) => {
     return (
         <div className="mt-20 w-full h-full flex justify-center">
-            <div className="md:w-[80%] w-[90%]">
-                {/* <div className="">
-                    <h1 className="text-2xl mt-5">
-                        {currentTab === "orders"
-                            ? "Your Orders"
-                            : currentTab === "profile"
-                            ? "Your Profile"
-                            : currentTab === "addresses"
-                            ? "Your Address"
-                            : null}
-                    </h1>
-                </div> */}
-                {children}
-            </div>
+            <div className="md:w-[80%] w-[90%]">{children}</div>
         </div>
     );
 };
--- client/src/components/account/profile/ProfileDisplay.jsx
+++ client/src/components/account/profile/ProfileDisplay.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const ProfileDisplay = () => {
-    return <div>ProfileDisplay</div>;
+    const { user } = useSelector((state) => state.auth);
+
+    const userSinceYear = user.createdAt.slice(0, 4);
+    const numOfOrders = user.orders.length;
+    const totals = [];
+
+    const totalMoneySpent = () => {
+        user.orders.forEach((order) => {
+            totals.push(parseFloat(order.total));
+        });
+
+        return totals.reduce((acc, curr) => acc + curr, 0).toFixed(2);
+    };
+    useEffect(() => {
+        console.log(userSinceYear);
+    });
+    return (
+        <div>
+            <div>
+                <h1 className="text-2xl">Hi, {user.firstName} </h1>
+                <h1 className="text-base text-neutral-400">
+                    User since {userSinceYear}
+                </h1>
+            </div>
+            <div className="mt-5">
+                <h1 className="text-xl">Your stats</h1>
+                <h1 className="text-neutral-400">
+                    Orders placed: {numOfOrders} orders
+                </h1>
+                <h1 className="text-neutral-400">
+                    Total spent: ${totalMoneySpent()}
+                </h1>
+            </div>
+        </div>
+    );
 };
 
 export default ProfileDisplay;
